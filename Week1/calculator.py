39ff7bde969e0eda684419f76a9467849d982ecb
Gab-u <gabrielsakwa@gmail.com>
2025-08-01 23:14:32 +0300

    Edited the calculator to make it more efficient

--- Week1/calculator.py
+++ Week1/calculator.py
@@ -1,14 +1,17 @@
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def mul(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+
+operators = {"+": add, "-": sub, "*": mul, "/": div}
+
 num1 = float(input("Enter your first number: "))
 operator = input("Operation: ")
 num2 = float(input("Enter your second number: "))
 
-if operator == "+":
-    print(f"{num1} {operator} {num2} = {num1 + num2}")
-elif operator == "-":
-    print(f"{num1} {operator} {num2} = {num1 - num2}")
-elif operator == "*":
-    print(f"{num1} {operator} {num2} = {num1 * num2}")
-elif operator == "/":
-    print(f"{num1} {operator} {num2} = {num1 / num2}")
-else:
-    print(f"{operator} is not a valid operator")
+result = operators[operator](num1, num2)
+print(f"{num1} {operator} {num2} = {result}")
